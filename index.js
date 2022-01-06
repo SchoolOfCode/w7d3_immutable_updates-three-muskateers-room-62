f2502417ae9fae33eb3ddc42a41e83341caee99f
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-01-06 12:31:54 +0000

    Almost done

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ export function replaceItem(array, item, index) {
 }
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+        return [...array.slice(0, index), ...array.slice(index+1)] 
+}
 
 //Objects:
 
@@ -32,7 +34,9 @@ export function removeItem(array, index) {}
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+    return {...object, name: newName};
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -40,7 +44,9 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+    return {...object, needsACupOfTea:!object.needsACupOfTea};
+}
 
 // Combo Time!!
 
@@ -50,4 +56,8 @@ export function toggleTeaStatus(object) {}
 //    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+export function toggleListItemCompleted(array, index) {
+    return [...array, ...array[index].completed:!array[index].completed
+        
+    ]
+}
